1e75e65dd48e5ebdc8c3fa51a78d65cd4dd2c704
Connor <connorwong93@gmail.com>
2019-10-16 14:24:45 -0700

    trying to get API authorization configured

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
 'use strict';
+
+function getUserRepos() {
+    $('#repo-form').submit(function(event) {
+        event.preventDefault();
+        $('.repos').empty();
+        let user = $('.candidate-entry').val();
+        console.log(user);
+        fetch('https://github.com/users/:'+ user +'/repos')
+            .then(response => response.json())
+            .then(responseJson => {
+                if (responseJson.code === 404) {
+                    alert("Try again...");
+                  }else{
+                    displayResults(responseJson);
+                  }
+            })
+    })
+}
+
+function displayResults(responseJson){
+    console.log(responseJson);
+    $('.repos').append(
+        `test..test..test` 
+    )
+    $('.results').removeClass('hidden');
+}
+
+function watchForm() {
+    getUserRepos();
+};
+
+$(function() {
+    console.log('App has loaded!');
+    watchForm();
+});
